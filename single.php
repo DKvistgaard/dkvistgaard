d1bacf6aa618cf5113dfc3d22a9514189264d548
Daniel Kvistgaard <dk@html24.net>
2014-03-22 20:55:37 +0100

    Implementing single post.

--- single.php
+++ single.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
 
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+    ?>
+            <main class="content">
+                <div class="container">
+                    <?php the_content(); ?>
+                </div>
+            </main>
+    <?php
+        }
+    }
+    ?>
+
 <?php get_footer(); ?>
