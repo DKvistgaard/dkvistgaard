6713ebdbc012e2d2918f8c365d31b3fc1d3ef80e
Daniel Kvistgaard <dk@html24.net>
2014-03-22 21:34:15 +0100

    Added meta viewport tag.

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui">
+    
 	<link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" href="http://brick.a.ssl.fastly.net/Roboto:250,500/Open+Sans:600/Alegreya+Sans:250i">
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory'); ?>/css/main.css" />
