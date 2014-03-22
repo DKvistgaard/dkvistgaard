a2c7f4b26c76f93a2f8589b5ca15360c14d8cc1c
Daniel Kvistgaard <dk@html24.net>
2014-03-22 20:28:24 +0100

    Implementing footer.

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
-<?php wp_footer(); ?>
+
+    <footer class="main-foot">
+        <p>
+            Created by Daniel Kvistgaard<br>
+            Lead Frontend Developer at HTML24<br>
+            &copy;<?php echo date('Y') ?>
+        </p>
+    </footer>
+
+    <?php wp_footer(); ?>
+
 </body>
 </html>
