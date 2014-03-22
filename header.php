e645c07ccf6925427a445f775285ddbda78eac64
Daniel Kvistgaard <dk@html24.net>
2014-03-22 21:08:40 +0100

    Fixing font-weights.
    The bold didn't work.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="stylesheet" href="http://brick.a.ssl.fastly.net/Roboto:250/Open+Sans:600/Alegreya+Sans:250i">
+    <link rel="stylesheet" href="http://brick.a.ssl.fastly.net/Roboto:250,500/Open+Sans:600/Alegreya+Sans:250i">
 	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory'); ?>/css/main.css" />
 	
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
