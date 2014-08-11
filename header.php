153017ba002a6878c9a9c4b0b27de7261d6b6763
Daniel Kvistgaard <dk@html24.net>
2014-08-11 18:16:11 +0200

    Changing fonts.
    Changing them to serif fonts to make it all more readable.

--- header.php
+++ header.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html> 
 <html lang="da"> 
 <head> 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta charset="utf-8">
-    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, minimal-ui">
-    
-	<link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="stylesheet" href="http://brick.a.ssl.fastly.net/Roboto:250,500/Open+Sans:600/Alegreya+Sans:250i">
-	<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory'); ?>/css/main.css" />
-	
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	
-	<!-- This script makes the HTML5 tags readable for IE -->
-	<!--[if lt IE 9]> <script src="<?php bloginfo('template_directory'); ?>/js/html5shiv.js"></script> <![endif]-->
+
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="stylesheet" href="//brick.a.ssl.fastly.net/Roboto+Slab:200,700/Andada:400,400i,700,700i">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('template_directory'); ?>/css/main.css" />
+
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+    <!-- This script makes the HTML5 tags readable for IE -->
+    <!--[if lt IE 9]> <script src="<?php bloginfo('template_directory'); ?>/js/html5shiv.js"></script> <![endif]-->
 	
     <?php wp_head(); ?>
     
